aeb8b0a1668fed709d8782b0c72480b93923ecdc
Victor Hong <vhoncito@gmail.com>
2024-12-09 18:46:33 +0000

    first commit

---
+++ src/main/java/org/hong/control_vehiculos/ControlVehiculosApplication.java
@@ -0,0 +1,13 @@
+package org.hong.control_vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hong/control_vehiculos/ControlVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hong.control_vehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
