d0c96900792a7bd0350c56598e94aaf050208e5f
Hong <vhoncito@gmail.com>
2025-02-05 11:55:59 +0000

    volviendo al formato original en frontend

--- src/main/java/org/hong/control_vehiculos/entity/Control_Vehiculos.java
+++ src/main/java/org/hong/control_vehiculos/entity/Control_Vehiculos.java
@@ -1,5 +1,6 @@
 package org.hong.control_vehiculos.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
